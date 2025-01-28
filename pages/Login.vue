ad1e57284efaab443b54a681f6858e60a9f910f3
Gavin Sokolof <sokolofg@gmail.com>
2025-01-27 21:10:46 -0500

    FEATURE/START-01 setup pages

---
+++ pages/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex-col content-center">
+    <LoginModal/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h5>homepage</h5>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
